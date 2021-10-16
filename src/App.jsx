e78fd0e0a586db5810aa93bb34afd8d498f84259
dchandra <chndraja@gmail.com>
2021-10-16 13:13:15 +0530

    basic routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import HomeScreen from "./components/HomeScreen";
+import { BrowserRouter as Router , Route, Switch } from "react-router-dom";
+
+import HomeScreen from "./screens/HomeScreen";
+import Navbar from "./components/Navbar";
+
 
 const App = () => {
   return (
     <div className="App">
-      <p className="fs-1">React---- Ecommerce Project</p>
-      <HomeScreen />
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora odio aut
-      doloribus magnam repellendus tenetur, nobis iusto voluptates dolores non!
-      Doloribus sit tempore ducimus nam sunt laudantium dolor voluptas quaerat!
+      <Router>
+        <Navbar/>
+        <Switch >
+          <Route path="/">
+            <HomeScreen/>
+          </Route>
+        </Switch>
+      </Router>
+     
     </div>
   );
 };
