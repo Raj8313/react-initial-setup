6e7447dcb37e9ab5d4ef96373aac159414d4052e
Raj8313 <dvrrajashekhar@gmail.com>
2021-10-18 12:37:32 +0530

    redux setup done

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,16 @@
+const initialState = {
+  loading: false,
+  error: false,
+  success: false,
+  errorMessage: false,
+  data: {},
+};
+
+const productReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default productReducer;
