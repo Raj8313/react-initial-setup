ecb8228f4da1efa05a9a63a6f237c71c67e5a0ed
Raj8313 <dvrrajashekhar@gmail.com>
2021-10-20 13:16:38 +0530

    env updated

--- src/redux/API/AxiosInstance.js
+++ src/redux/API/AxiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const AxiosInstance = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
   params: {
-    key: process.env.REACT_APP_KEY,
+    key: process.env.REACT_APP_API_KEY,
   },
 });
 
